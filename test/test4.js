514ac49cc2c671b177e2ac195dfb0378e8255ef8
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2021-10-07 09:08:17 +1100

    Create test4.js

---
+++ test/test4.js
@@ -0,0 +1,10 @@
+//var assert = require('assert');
+import assert from 'assert'
+describe('Describe 4', function() {
+  describe('sub Describe 4', function() {
+    it('it 3', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+      assert.equal(["1"], -1);
+    });
+  });
+});
